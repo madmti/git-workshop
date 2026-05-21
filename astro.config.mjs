4ad98df69c909ba2bae3b87254e1cebbc0b54d35
madmti <supermatrix9@gmail.com>
2026-05-21 14:36:09 -0400

    chore: base project

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+	output: 'static',
+    site: 'https://madmti.github.io',
+    base: '/git-workshop',
+	compressHTML: true,
+	integrations: [sitemap(), mdx()],
+
+	vite: {
+		plugins: [tailwindcss()]
+	}
+});
